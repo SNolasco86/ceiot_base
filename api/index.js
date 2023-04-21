0d8a5fad98b248973e72495a996cdddb41b90d37
SNolasco86 <santiagonolasco86@gmail.com>
2023-04-21 23:23:30 +0000

    entrega 21 abril

--- api/index.js
+++ api/index.js
@@ -43,8 +43,8 @@ app.use(express.static('spa/static'));
 const PORT = 8080;
 
 app.post('/measurement', function (req, res) {
--       console.log("device id    : " + req.body.id + " key         : " + req.body.key + " temperature : " + req.body.t + " humidity    : " + req.body.h);	
-    const {insertedId} = insertMeasurement({id:req.body.id, t:req.body.t, h:req.body.h});
+-       console.log("device id    : " + req.body.id + " key         : " + req.body.key + " temperature : " + req.body.t +" pressure : "+ req.body.p+ " humidity    : " + req.body.h);	
+    const {insertedId} = insertMeasurement({id:req.body.id, t:req.body.t,p:req.body.p, h:req.body.h});
 	res.send("received measurement into " +  insertedId);
 });
 
